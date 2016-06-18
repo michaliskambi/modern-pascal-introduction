d660ef3bcba47c2a3f56bbbd46559c93ec0880d8
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-06-18 20:13:25 +0200

    Preprocessor section. Using units from units, circular dependencies. Move "output" section higher.

---
+++ anotherunit.pas
@@ -0,0 +1,23 @@
+{$mode objfpc}{$H+}
+unit AnotherUnit;
+interface
+
+uses Classes;
+
+{ The "TComponent" type (class) is defined in the Classes unit.
+  That's why we had to use the Classes unit above. }
+procedure DoSomethingWithComponent(var C: TComponent);
+
+implementation
+
+uses SysUtils;
+
+procedure DoSomethingWithComponent(var C: TComponent);
+begin
+  { The FreeAndNil procedure is defined in the SysUtils unit.
+    Since we only refer to it's name in the implementation,
+    it was OK to use the SysUtils unit in the "implementation" section. }
+  FreeAndNil(C);
+end;
+
+end.
