8659fd6b9c0a5a2fb04a70114d83ec3f2cc28ea1
eugeneloza <Loza@i.ua>
2017-02-24 13:53:55 +0200

    initialization - finalization in Units

---
+++ code-samples/initialization_finalization.pas
@@ -0,0 +1,15 @@
+{$mode objfpc}{$H+}{$J-}
+unit initialization_finalization;
+interface
+
+implementation
+
+initialization
+
+writeln('Hello world!');
+
+finalization
+
+writeln('Goodbye world!');
+
+end.
